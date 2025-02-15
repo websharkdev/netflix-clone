3bf6e03390f236121db3467771b37ae2abab8b34
Oleksii <alexey.bortnytskyi@gmail.com>
2025-02-15 17:48:15 +0100

    refactor(components/providers) - added new <Provider> > <UseQueryProvider>

--- components/providers/main.provider.tsx
+++ components/providers/main.provider.tsx
@@ -1,18 +1,18 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "./theme.provider";
 
 type Props = {
   children: Readonly<React.ReactNode>;
 };
 
+const queryClient = new QueryClient();
+
 const MainProvider = ({ children }: Props) => {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      {children}
+    <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </ThemeProvider>
   );
 };
