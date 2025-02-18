2a422231c6ed98fa5d7e5a60d492993e856609b0
Oleksii <alexey.bortnytskyi@gmail.com>
2025-02-18 15:12:33 +0100

    feature(components/ui) - removed ScrollType (because of error - @typescript-eslint/no-empty-object-type), also added bigger size of <ScrollBar>

--- components/ui/scroll-progress.tsx
+++ components/ui/scroll-progress.tsx
@@ -3,12 +3,10 @@
 import { cn } from "@/lib/utils";
 import { motion, MotionProps, useScroll } from "motion/react";
 import React from "react";
-interface ScrollProgressProps
-  extends Omit<React.HTMLAttributes<HTMLElement>, keyof MotionProps> {}
 
 export const ScrollProgress = React.forwardRef<
   HTMLDivElement,
-  ScrollProgressProps
+  Omit<React.HTMLAttributes<HTMLElement>, keyof MotionProps>
 >(({ className, ...props }, ref) => {
   const { scrollYProgress } = useScroll();
 
@@ -16,7 +14,7 @@ export const ScrollProgress = React.forwardRef<
     <motion.div
       ref={ref}
       className={cn(
-        "fixed inset-x-0 top-0 z-50 h-px origin-left bg-gradient-to-r from-[#A97CF8] via-[#F38CB8] to-[#FDCC92]",
+        "fixed inset-x-0 top-0 z-50 h-1 origin-left bg-gradient-to-r from-[#A97CF8] via-[#F38CB8] to-[#FDCC92]",
         className
       )}
       style={{
