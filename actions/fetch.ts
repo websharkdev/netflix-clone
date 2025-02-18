1b6ce7e4e4a491417a976d722e859bc411f1f9bb
Oleksii <alexey.bortnytskyi@gmail.com>
2025-02-18 13:40:43 +0100

    feature(actions/fetch) - fixed <axios> fetch url

--- actions/fetch.ts
+++ actions/fetch.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 
 export const getMovies = async () => {
     try {
-        const result = await axios("http://localhost:3000/api/dashboard").then((res) => {
+        const result = await axios((process.env.BETTER_AUTH_URL as string) + "/api/dashboard").then((res) => {
             return res.data
         })
         return result.data
