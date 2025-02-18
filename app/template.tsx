91d4d7819daf9301bf6b5329be5caea8b44e4622
Oleksii <alexey.bortnytskyi@gmail.com>
2025-02-18 15:08:25 +0100

    refactor(app) - removed unused components & paths

--- app/template.tsx
+++ app/template.tsx
@@ -21,7 +21,7 @@ const Template = ({ children }: Props) => {
     <div className="bg-stone-50 dark:bg-black h-full w-full min-h-screen text-stone-900 dark:text-white">
       <ScrollProgress />
 
-      <header className="mx-auto z-50 bg-stone-50/40 dark:bg-black/40 backdrop-blur-lg px-6 py-4 rounded-xl container flex items-center flex-nowrap justify-between">
+      <header className="mx-auto z-50 bg-stone-50/40 dark:bg-black/40 backdrop-blur-lg px-6 py-4 rounded-xl container flex items-center flex-nowrap justify-between relative">
         <Link
           href="/"
           className="font-bold text-red-600 dark:text-white text-lg uppercase"
@@ -30,7 +30,7 @@ const Template = ({ children }: Props) => {
         </Link>
 
         {!!data?.session ? (
-          <div className="text-sm font-medium flex justify-between gap-10 items-center transition-all duration-500">
+          <div className="text-sm font-medium flex justify-between gap-10 items-center transition-all duration-500 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
             <Link
               className="transition-all duration-500 text-stone-500 hover:text-stone-950"
               href="/dashboard"
@@ -48,12 +48,6 @@ const Template = ({ children }: Props) => {
               href="/dashboard?t=movie"
             >
               Movies
-            </Link>
-            <Link
-              className="transition-all duration-500 text-stone-500 hover:text-stone-950"
-              href="/dashboard/watchlist"
-            >
-              Watchlist
             </Link>
           </div>
         ) : null}
